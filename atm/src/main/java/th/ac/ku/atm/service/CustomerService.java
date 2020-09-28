b8ee65b500c93879fdd90ef1e04d8c2e70561eab
wanchaiJ <wanchai.jan@ku.th>
2020-09-28 21:51:19 +0700

    My Lab7-1 JDBC DB Connect

--- atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -10,18 +12,24 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    //private List<Customer> customerList;
+    private CustomerRepository repository;
 
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
+
+//    @PostConstruct
+//    public void postConstruct() {
+//        this.customerList = new ArrayList<>();
+//    }
 
     public void createCustomer(Customer customer) {
         //... hash pin for customer ...
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
+        //customerList.add(customer);
     }
 
     private String hash(String pin) {
@@ -31,15 +39,22 @@ public class CustomerService {
 
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+
+        //return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+//        for (Customer customer : customerList) {
+//            if (customer.getId() == id)
+//                return customer;
+//        }
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
+
     }
 
     public Customer checkPin(Customer inputCustomer) {
